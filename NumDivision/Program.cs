5c4943da90ab48ef7bebf25697f8b60ad51ca430
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-15 14:22:38 +0100

    Start of week 3

---
+++ NumDivision/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pjestim dy nr");
+            Console.WriteLine("=====RREGULLAT=====\n" +
+                "1. Nje nr nuk mund te pjestohet me zero");
+            Console.WriteLine("Vendos dy numra per tu pjestuar: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int total = num1 / num2;
+                Console.WriteLine(total);
+            }
+
+            catch (System.DivideByZeroException e){
+                Console.WriteLine(String.Concat(e.Message, e.StackTrace));
+                Console.WriteLine("Nuk lejohet nr te pjestohet me zero");
+            }
+        }
+    }
+}
---
+++ Week3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\source\repos\FileArray\Text\test.txt";
+            string[] arr;
+            int lines;
+
+            Console.WriteLine("File u krijuar");
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            Console.WriteLine("Sa rrjeshta string do qe te shkruhen ne file");
+            lines = Convert.ToInt32(Console.ReadLine());
+            arr = new string[lines];
+            Console.WriteLine("Shkruaj {0} strings: ", lines);
+            for(int i=0; i<lines; i++)
+            {
+                Console.WriteLine("Vendos rrjeshtin e {0} :", i + 1);
+                arr[i] = Console.ReadLine();
+            }
+
+            System.IO.File.WriteAllLines(path, arr);
+        }
+    }
+}
