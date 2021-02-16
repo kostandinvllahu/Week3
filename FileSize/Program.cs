c892f99cc5587888d3f643644bc3359404d07370
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-16 20:31:33 +0100

    Test

---
+++ FileSize/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string filesize = @"C:\Users\User\Desktop\lendet.txt";
+            Console.WriteLine("Calculating size of file");
+            FileInfo info = new FileInfo(filesize);
+            long length = info.Length;
+            Console.WriteLine("Length in Bytes {0} ", length);
+            //End of the program
+        }
+    }
+}
---
+++ OnlineAttendance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OnlineAttendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Online Attendance Program");
+            string name;
+            Console.WriteLine("Enter name: ");
+            name = Console.ReadLine();
+
+            if (name == "Jack" || name == "Steven" || name == "Mathew")
+            {
+                Console.WriteLine("User is baned");
+                Console.WriteLine("Sending email to administrator");
+                Console.WriteLine("Fire alarm started");
+                for (; ;) //infinit loop
+                {
+                    Console.Beep();
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            else
+            {
+                Console.WriteLine("WELCOME " + name);
+            }
+
+        }
+    }
+}
