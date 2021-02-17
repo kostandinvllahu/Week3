18e09314d60729c68ef84c59b8e15ca8a8cc1acb
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-17 10:26:01 +0100

    Cartesian Finished

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Number to the N");
+            int num = 0;
+            Console.WriteLine("Vendos nr e N");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 2; i<num; i++)
+            {
+                bool isPrime = true;
+               for(int j=2; j<i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                    
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
