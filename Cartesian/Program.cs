fcf2038cc6cf5f9a8b1caef0e665e77d9c03d34c
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-17 09:13:53 +0100

    Starting doing the cartesian

---
+++ Cartesian/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cartesian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
